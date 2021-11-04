649333ef264641cd98c6291eec59d371c4f0f0c8
Danny Monte <67159240+diirenek2@users.noreply.github.com>
2021-11-04 13:08:20 -0300

    dd

--- config/mail.php
+++ config/mail.php
@@ -36,11 +36,11 @@ return [
     'mailers' => [
         'smtp' => [
             'transport' => 'smtp',
-            'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
-            'port' => env('MAIL_PORT', 587),
+            'host' => env('MAIL_HOST', 'smtp.hostinger.com'),
+            'port' => env('MAIL_PORT', 465),
             'encryption' => env('MAIL_ENCRYPTION', 'tls'),
-            'username' => env('MAIL_USERNAME'),
-            'password' => env('MAIL_PASSWORD'),
+            'username' => env('info@devweb.com.ar'),
+            'password' => env('Grillo1495'),
             'timeout' => null,
             'auth_mode' => null,
         ],
