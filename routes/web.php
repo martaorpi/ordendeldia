6a459166695977aca081b8f691242a4f04e8fecc
Danny Monte <67159240+diirenek2@users.noreply.github.com>
2021-11-05 20:43:04 -0300

    verified

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth', 'verify'])->name('dashboard');
+})->middleware(['auth','verified'])->name('dashboard');
 
 require __DIR__.'/auth.php';
+
+Route::post('formulario-inscripcion', [Controller::class, 'student_update']);
+
+
