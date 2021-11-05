b557eaf2911072a34d0b3de8b397e23fa1d6a19f
Danny Monte <67159240+diirenek2@users.noreply.github.com>
2021-11-05 17:43:46 -0300

    verificado

--- routes/web.php
+++ routes/web.php
@@ -17,10 +17,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+})->middleware(['auth','verified'])->name('dashboard');
 
 require __DIR__.'/auth.php';
 
