c6ba115106f9c65b5b7dc11b71ce9ba9d3c1f052
Danny Monte <67159240+diirenek2@users.noreply.github.com>
2021-11-08 17:26:01 -0300

    dd

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,13 +1,10 @@
-@php
-    $formulario = App\Models\Student::where('user_id',auth()->user()->id)->get();
-@endphp
 <x-app-layout>
     <x-slot name="header"></x-slot>
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    @if(!empty($formulario))
+                    @if(!empty(auth()->user()->student[0]))
                         <b class="text-grey h2">Su solicitud está siendo procesada</b>
                         <i class="fas fa-check-circle fa-2x text-success ml-2"></i>
                         <a href="creatempdf?id=4" target="_blank" class="btn btn-md btn-primary login-submit-cs">Imprimir Formulario de Reinscipción</a>
