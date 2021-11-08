fe593ff89fe7269891eaf500638b2654da24cb10
anavirginiacres <67160931+anavirginiacres@users.noreply.github.com>
2021-11-08 12:32:28 -0300

    expire

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -42,11 +42,13 @@ class AppServiceProvider extends ServiceProvider
         
         ResetPassword::$toMailCallback = function($notifiable, $url) {
             return (new MailMessage)
+            ->greeting('Hola!')
             ->subject(Lang::get('Restablecimiento de Contraseña'))
             ->line(Lang::get('Recibimos una solicitud de restablecimiento de contraseña para tu cuenta.'))
             ->action(Lang::get('Restablecer Contraseña'), $url)
-            //->line(Lang::get('This password reset link will expire in :count minutes.', ['count' => config('auth.passwords.'.config('auth.defaults.passwords').'.expire')]))
-            ->line(Lang::get('Si no solicitaste un restablecimiento de contraseña, no es necesario realizar ninguna otra acción.'));
+            ->line(Lang::get('This password reset link will expire in :count minutes.', ['count' => config('auth.passwords.'.config('auth.defaults.passwords').'.expire')]))
+            ->line(Lang::get('Si no solicitaste un restablecimiento de contraseña, no es necesario realizar ninguna otra acción.'))
+            ->salutation(new HtmlString('Quedamos a tu disposición,<br><strong>Equipo ISMP</strong>'));
         };
     }
 }
