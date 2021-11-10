a0d26a749687119e2299eaee9001438dc6eed4a2
anavirginiacres <67160931+anavirginiacres@users.noreply.github.com>
2021-11-10 10:51:15 -0300

    middleware

--- routes/backpack/custom.php
+++ routes/backpack/custom.php
@@ -9,7 +9,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::group([
     'prefix'     => config('backpack.base.route_prefix', 'admin'),
-    'middleware' => ['web', 'admin', 'can:administracion_academica'],
+    'middleware' => ['web', 'admin'],
+    //'middleware' => ['web', 'admin', 'can:administracion_academica'],
     'namespace'  => 'App\Http\Controllers\Admin',
 ], function () { // custom admin routes
     Route::crud('examenes', 'ExamCrudController');
