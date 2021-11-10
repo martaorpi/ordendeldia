ce40cc6660acb700a1f782e51dc85ea2eb39ee8a
Danny Monte <67159240+diirenek2@users.noreply.github.com>
2021-11-09 22:03:54 -0300

    permissions

--- routes/backpack/permissionmanager.php
+++ routes/backpack/permissionmanager.php
@@ -14,7 +14,7 @@ Route::group([
     'namespace'  => 'Backpack\PermissionManager\app\Http\Controllers',
     'prefix'     => config('backpack.base.route_prefix', 'admin'),
     //'middleware' => ['web', backpack_middleware()],
-    'middleware' => ['web', 'admin', 'can:administracion_de_usuarios'],
+    'middleware' => ['web', 'admin', 'can:administracion_del_personal'],
 ], function () {
     Route::crud('permission', 'PermissionCrudController');
     Route::crud('role', 'RoleCrudController');
