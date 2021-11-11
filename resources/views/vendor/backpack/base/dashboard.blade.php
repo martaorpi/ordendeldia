a63b1a8b665cdd23492de9a9bf17b0d385547069
anavirginiacres <67160931+anavirginiacres@users.noreply.github.com>
2021-11-11 10:26:37 -0300

    dashboard

--- resources/views/vendor/backpack/base/dashboard.blade.php
+++ resources/views/vendor/backpack/base/dashboard.blade.php
@@ -2,6 +2,13 @@
 
 @section('content')
 
-ssss
-
+@php
+    $widgets['before_content'][] = [
+        'type'        => 'jumbotron',
+        'heading'     => trans('backpack::base.welcome'),
+        'content'     => trans('backpack::base.use_sidebar'),
+        'button_link' => backpack_url('logout'),
+        'button_text' => trans('backpack::base.logout'),
+    ];
+@endphp
 @endsection
