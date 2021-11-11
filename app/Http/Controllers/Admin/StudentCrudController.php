5c6c284fb1f48ffc2e087dcedeee9972d3288697
Danny Monte <67159240+diirenek2@users.noreply.github.com>
2021-11-11 18:03:03 -0300

    send mail personalizado

--- app/Http/Controllers/Admin/StudentCrudController.php
+++ app/Http/Controllers/Admin/StudentCrudController.php
@@ -206,15 +206,103 @@ class StudentCrudController extends CrudController
         $log->user_admin_id = auth()->user()->id;
         $log->student_id = $id;
         $log->text = $input['val'];
-        $log->type = 'Rechazados';
+        $log->type = 'Observacion enviada';
         $log->save();
         
-        $student->status = 'Rechazado';
-        if($student->save()){
+        //$student->status = 'Rechazado';
+        /*if($student->save()){
             return ["status" => $input];
         }else{
             return ["status" => 400];
-        }
+        }*/
+        $email_destino = $student->user->email;
+
+        $cuerpo = '<style>
+            .footer-copyright-area {
+                background: linear-gradient(178deg, #e12503 0%, #85060c 100%);
+                padding: 20px 0px 10px 0;
+                text-align: center;
+                color: #fff;
+                font-size: 12px;
+            }
+            .linea{
+                padding-top:0;
+                margin-top:0;
+                background: black;
+            }
+            .link{
+                color: #a52929;
+                text-decoration: underline;
+            }
+            .link:hover{
+                color: black;
+            }
+            </style>
+            <!DOCTYPE html>
+            <html>
+                <head>
+                    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+                    <title>ISMP</title>
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+                    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+                </head>
+                <body style="margin: 0; padding: 0;">
+                    <table align="center" border="0" cellpadding="0" cellspacing="0" width="500">
+                        <tr>
+                            <td align="center" bgcolor="#70bbd9">
+                                <img src="http://synergysoft.host/porres/img/cabecera_mail.jpeg" width="100%" height="35%" style="display: block;" />
+                            </td>
+                        </tr>
+                        <tr style="padding-bottom:0">
+                            <td align="left" bgcolor="#fff" style="border-left: 1px solid grey;border-right: 1px solid grey;">
+                                <img src="http://synergysoft.host/porres/img/instituto.jpg" width="150" style="margin-bottom:0"/>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td bgcolor="#ffffff" style="padding: 10px 30px 20px 30px;border-left: 1px solid grey;border-right: 1px solid grey;">
+                                <table border="0" cellpadding="0" cellspacing="0" width="100%">
+                                    <tr>
+                                        <td>
+                                            <p class="text-left">
+                                                <b>'.utf8_decode("Luego de realizar un análisis de la documentación presentada se observa lo siguiente:").'</b>
+                                            </p>
+                                        </td>
+                                    </tr>
+                                    <tr>   
+                                        <td align="right" style="padding-left:40%;padding-top:0">
+                                            <hr class="linea">
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            <p class="text-left" style="padding:0 10% 0 10%" id="observaciones">'.utf8_decode($input['val']).'</p>
+                                        </td>
+                                    </tr>
+                                    <tr>   
+                                        <td align="left" style="padding-right:40%;padding-top:0">
+                                            <hr class="linea">
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td align="center">
+                                            <br><i>Gracias por elegir el ISMP<br>
+                                            '.utf8_decode("Te deseamos éxitos en tu formación profesional.").'</i>
+                                        </td>
+                                    </tr>
+                                </table>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <div style="background: linear-gradient(178deg, #e12503 0%, #85060c 100%); padding: 20px 0px 10px 0; text-align: center; color: #fff; font-size: 12px;">
+                                    <p>Copyright &copy; '.date("Y").' <b>SinergySoft</b> Todos los derechos reservados.</p>
+                                </div>
+                            </td>
+                        </tr>
+                    </table> 
+                </body>
+            </html>';
+        $this->sendMail($email_destino,$cuerpo);
     }
 
     public function sendMail($email_destino,$cuerpo){
@@ -222,7 +310,7 @@ class StudentCrudController extends CrudController
         $asunto = "Informacion ISMP";
         $headers = "MIME-Version: 1.0\r\n"; 
         $headers .= "Content-type: text/html; charset=iso-8859-1\r\n"; 
-        $headers .= "From: ISMP Soporte <ismp_suport@synergysoft.host>\r\n"; 
+        $headers .= "From: ISMP Soporte <info@devweb.com.ar>\r\n"; 
 
         if($email_destino != ''){
             mail($email_destino, $asunto, $cuerpo, $headers);
@@ -401,7 +489,7 @@ class StudentCrudController extends CrudController
                         </body>
                     </html>';
 
-                    //$this->sendMail($email_destino,$cuerpo);
+                    $this->sendMail($email_destino,$cuerpo);
                     print_r($http_code);
                     break;
                 case 400:
