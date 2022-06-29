3caeccfcb34955218ad3b80bce09773fe02d05d3
martaorpi <67160498+martaorpi@users.noreply.github.com>
2022-06-28 21:10:43 -0300

    puntaje por planta

--- resources/views/novedades.blade.php
+++ resources/views/novedades.blade.php
@@ -174,7 +174,7 @@
                                             <div class="accordian-body collapse" id="demo{{$license->id}}"> 
                                                 <table class="table table-striped m-4">
                                                     <thead class="table-danger">
-                                                        <tr class="red-text">
+                                                        <tr>
                                                             <th>#</th>
                                                             <th>Personal</th>
                                                             <th>Privada</th>
@@ -213,14 +213,14 @@
                 </div>
                 <div class="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab">
                     <table class="table table-condensed responsive" style="border-collapse:collapse;">
-                        <thead>
+                        <thead class="table-danger">
                             <tr>
                                 <th></th>
                                 <th>Función</th>
-                                <th>Privada</th>
-                                <th>Suplente SPEP</th>
-                                <th>Titular SPEP</th>
-                                <th>Total General</th>
+                                <th class="text-center">Privada</th>
+                                <th class="text-center">Suplente SPEP</th>
+                                <th class="text-center">Titular SPEP</th>
+                                <th class="text-center">Total General</th>
                             </tr>
                         </thead>
                         <tbody>
@@ -254,60 +254,150 @@
                                 $i=1;
                                 @endphp
                                 <tr data-toggle="collapse" data-target="#demo{{$job->job_id}}" class="accordion-toggle">
-                                    <td width="5%"><button class="btn btn-default btn-xs"><span class="glyphicon glyphicon-eye-open"></span></button></td>
+                                    <td width="5%">@if($privada + $sup_spep + $tit_spep > 0)<button class="btn btn-default btn-xs py-0 px-2">Ver</button>@endif</td>
                                     <td>{{ $j->description }}</td>
-                                    <td>{{ $privada }}</td>
-                                    <td>{{ $sup_spep }}</td>
-                                    <td>{{ $tit_spep }}</td>
-                                    <td>{{ $privada + $sup_spep + $tit_spep }}</td>
+                                    <td class="text-center">{{ $privada }}</td>
+                                    <td class="text-center">{{ $sup_spep }}</td>
+                                    <td class="text-center">{{ $tit_spep }}</td>
+                                    <td class="table-danger text-center">{{ $privada + $sup_spep + $tit_spep }}</td>
                                 </tr>
                                 {{------------------COLLAPSE OCULTO---------------}}
                                 @if ($privada + $sup_spep + $tit_spep > 0)
                                     <tr>
-                                        <td colspan="12" class="hiddenRow"><div class="accordian-body collapse" id="demo{{$job->job_id}}"> 
-                                            <table class="table table-striped">
-                                                <thead class="thead-dark">
-                                                    <tr class="red-text">
-                                                        <th>#</th>
-                                                        <th>Personal</th>
-                                                        <th>Privada</th>
-                                                        <th>Suplente SPEP</th>
-                                                        <th>Titular SPEP</th>
-                                                    </tr>
-                                                </thead>
-                                                <tbody>
-                                                    @foreach ($staff_jobs as $staff_job)
+                                        <td colspan="5" class="hiddenRow p-0">
+                                            <div class="accordian-body collapse" id="demo{{$job->job_id}}"> 
+                                                <table class="table table-striped m-4">
+                                                    <thead class="table-danger">
                                                         <tr>
-                                                            <td>{{ $i++ }}</td>
-                                                            <td>{{$staff_job->staff->name}}</td>
-                                                            <td>@if($staff_job->plant_type == 'PRIVADA') {{$staff_job->weekly_hours}} @endif</td>
-                                                            <td>@if($staff_job->plant_type == 'SUPLENTE SPEP') {{$staff_job->weekly_hours}} @endif</td>
-                                                            <td>@if($staff_job->plant_type == 'TITULAR SPEP') {{$staff_job->weekly_hours}} @endif</td>
+                                                            <th>#</th>
+                                                            <th>Personal</th>
+                                                            <th>Privada</th>
+                                                            <th>Suplente SPEP</th>
+                                                            <th>Titular SPEP</th>
                                                         </tr>
-                                                    @endforeach
-                                                </tbody>
-                                            </table>
+                                                    </thead>
+                                                    <tbody>
+                                                        @foreach ($staff_jobs as $staff_job)
+                                                            <tr>
+                                                                <td>{{ $i++ }}</td>
+                                                                <td>{{$staff_job->staff->name}}</td>
+                                                                <td>@if($staff_job->plant_type == 'PRIVADA') {{$staff_job->weekly_hours}} @endif</td>
+                                                                <td>@if($staff_job->plant_type == 'SUPLENTE SPEP') {{$staff_job->weekly_hours}} @endif</td>
+                                                                <td>@if($staff_job->plant_type == 'TITULAR SPEP') {{$staff_job->weekly_hours}} @endif</td>
+                                                            </tr>
+                                                        @endforeach
+                                                    </tbody>
+                                                </table>
+                                            </div>
                                         </td>
                                     </tr>
                                 @endif
                                 {{-------------------------------------------------}}
                             @endforeach
-                            <tr>
+                            <tr class="table-danger">
                                 <th></th>
                                 <th>Total General</th>
-                                <td>{{ $priv_gral }}</td>
-                                <td>{{ $sup_spep_gral }}</td>
-                                <td>{{ $tit_spep_gral }}</td>
-                                <td>{{ $priv_gral + $sup_spep_gral + $tit_spep_gral }}</td>
+                                <td class="text-center">{{ $priv_gral }}</td>
+                                <td class="text-center">{{ $sup_spep_gral }}</td>
+                                <td class="text-center">{{ $tit_spep_gral }}</td>
+                                <th class="text-center">{{ $priv_gral + $sup_spep_gral + $tit_spep_gral }}</th>
                             </tr>
                         </tbody>
                     </table>
                 </div>
                 <div class="tab-pane fade" id="pills-sanciones" role="tabpanel" aria-labelledby="pills-sanciones-tab">
-                    
-                </div>
-                   
-            </div>
+                    <table class="table table-condensed responsive" style="border-collapse:collapse;">
+                        <thead class="table-danger">
+                            <tr>
+                                <th></th>
+                                <th>Función</th>
+                                <th class="text-center">Privada</th>
+                                <th class="text-center">Suplente SPEP</th>
+                                <th class="text-center">Titular SPEP</th>
+                                <th class="text-center">Total General</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @php
+                            $priv_gral = 0;                                
+                            $sup_spep_gral = 0;                                
+                            $tit_spep_gral = 0;                                
+                            @endphp
+                            @foreach ($jobs as $job)
+                                @php
+                                $j = App\Models\Job::where('id', $job->job_id)->first();
+                                $privada = App\Models\StaffSubject::whereHas('staff', function($q){$q->where('status', 'Activo');})
+                                                ->where('job_id', $job->job_id)
+                                                ->where('plant_type', 'Privada')
+                                                ->sum('charge_score_ismp');
+                                $sup_spep = App\Models\StaffSubject::whereHas('staff', function($q){$q->where('status', 'Activo');})
+                                                ->where('job_id', $job->job_id)
+                                                ->where('job_id', $job->job_id)->where('plant_type', 'Suplente Spep')
+                                                ->sum('charge_score_ismp');
+                                $tit_spep = App\Models\StaffSubject::whereHas('staff', function($q){$q->where('status', 'Activo');})
+                                                ->where('job_id', $job->job_id)
+                                                ->where('job_id', $job->job_id)->where('plant_type', 'Titular Spep')
+                                                ->sum('charge_score_ismp');
+                                $priv_gral += $privada;
+                                $sup_spep_gral += $sup_spep;
+                                $tit_spep_gral += $tit_spep;
+                                $staff_jobs = App\Models\StaffSubject::whereHas('staff', function($q){$q->where('status', 'Activo');})
+                                                ->where('job_id', $job->job_id)
+                                                ->whereIn('plant_type', ['Privada', 'Suplente Spep', 'Titular Spep'])
+                                                ->get();
+                                $i=1;
+                                @endphp
+                                <tr data-toggle="collapse" data-target="#demo{{$job->job_id}}" class="accordion-toggle">
+                                    <td width="5%">@if($privada + $sup_spep + $tit_spep > 0)<button class="btn btn-default btn-xs py-0 px-2">Ver</button>@endif</td>
+                                    <td>{{ $j->description }}</td>
+                                    <td class="text-center">{{ $privada }}</td>
+                                    <td class="text-center">{{ $sup_spep }}</td>
+                                    <td class="text-center">{{ $tit_spep }}</td>
+                                    <td class="table-danger text-center">{{ $privada + $sup_spep + $tit_spep }}</td>
+                                </tr>
+                                {{------------------COLLAPSE OCULTO---------------}}
+                                @if ($privada + $sup_spep + $tit_spep > 0)
+                                    <tr>
+                                        <td colspan="5" class="hiddenRow p-0">
+                                            <div class="accordian-body collapse" id="demo{{$job->job_id}}"> 
+                                                <table class="table table-striped m-4">
+                                                    <thead class="table-danger">
+                                                        <tr>
+                                                            <th>#</th>
+                                                            <th>Personal</th>
+                                                            <th>Privada</th>
+                                                            <th>Suplente SPEP</th>
+                                                            <th>Titular SPEP</th>
+                                                        </tr>
+                                                    </thead>
+                                                    <tbody>
+                                                        @foreach ($staff_jobs as $staff_job)
+                                                            <tr>
+                                                                <td>{{ $i++ }}</td>
+                                                                <td>{{$staff_job->staff->name}}</td>
+                                                                <td>@if($staff_job->plant_type == 'PRIVADA') {{$staff_job->charge_score_ismp}} @endif</td>
+                                                                <td>@if($staff_job->plant_type == 'SUPLENTE SPEP') {{$staff_job->charge_score_ismp}} @endif</td>
+                                                                <td>@if($staff_job->plant_type == 'TITULAR SPEP') {{$staff_job->charge_score_ismp}} @endif</td>
+                                                            </tr>
+                                                        @endforeach
+                                                    </tbody>
+                                                </table>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                @endif
+                                {{-------------------------------------------------}}
+                            @endforeach
+                            <tr class="table-danger">
+                                <th></th>
+                                <th>Total General</th>
+                                <td class="text-center">{{ $priv_gral }}</td>
+                                <td class="text-center">{{ $sup_spep_gral }}</td>
+                                <td class="text-center">{{ $tit_spep_gral }}</td>
+                                <th class="text-center">{{ $priv_gral + $sup_spep_gral + $tit_spep_gral }}</th>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
