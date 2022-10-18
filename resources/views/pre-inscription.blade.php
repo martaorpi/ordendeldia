e40dceac590b6d272f996f143f527e690e8514a7
martaorpi <67160498+martaorpi@users.noreply.github.com>
2022-10-17 21:22:56 -0300

    formulario

--- resources/views/pre-inscription.blade.php
+++ resources/views/pre-inscription.blade.php
@@ -142,17 +142,39 @@
                     <input type="number" class="form-control" id="dni" name="dni" value="{{ old('dni')}}" required>
                 @endif
             </div>
-        </div>
-        
-        <div class="form-group row">
-            <div class="col-12">
-                <hr class="linea_bordo mb-4">
-                <h6><b>DOMICILIO</b></h6>
+
+            <div class="col-12 col-lg-6">
+                <label for="fecha_nac">Fecha de Nacimiento</label>
+                @if ($estudiante)
+                    <input type="date" class="form-control" id="fecha_nac" name="date_birth" value="{{ $estudiante->date_birth }}" required>
+                @else
+                    <input type="date" class="form-control" id="fecha_nac" name="date_birth" value="{{ old('date_birth')}}" required>
+                @endif
             </div>
         </div>
 
         <div class="form-group row">
             <div class="col-12 col-lg-6">
+                <label for="fijp">Teléfono Fijo</label>
+                @if ($estudiante)
+                    <input type="text" class="form-control" id="fijp" name="landline" value="{{ $estudiante->landline }}" required>
+                @else
+                    <input type="text" class="form-control" id="fijp" name="landline" value="{{ old('landline')}}" required>
+                @endif
+            </div>
+
+            <div class="col-12 col-lg-6">
+                <label for="celular">Teléfono Celular</label>
+                @if ($estudiante)
+                    <input type="text" class="form-control" id="celular" name="cell_phone" value="{{ $estudiante->cell_phone }}" required>
+                @else
+                    <input type="text" class="form-control" id="celular" name="cell_phone" value="{{ old('cell_phone')}}" required>
+                @endif
+            </div>
+        </div>
+
+        <div class="form-group row">
+            <div class="col-12">
                 <label for="nacionalidad">Nacionalidad</label>
                 <select class="form-control selectpicker" data-live-search="true" id="nacionalidad" name="nationality_id" required>
                     <option value="">Seleccione</option>
@@ -165,8 +187,18 @@
                     @endforeach
                 </select>
             </div>
+        </div>
         
-            <div class="col-12 col-lg-6">
+        <div class="form-group row">
+            <div class="col-12">
+                <hr class="linea_bordo mb-4">
+                <h6><b>DOMICILIO REAL (Domicilio en el que reside actualmente)</b></h6>
+            </div>
+        </div>
+
+        <div class="form-group row">
+            
+            <div class="col-12">
                 <label for="provincia">Provincia</label>
                 <select class="form-control selectpicker" data-live-search="true" id="provincia" name="province_id" disabled>
                     <option value="">Seleccione</option>
@@ -182,7 +214,7 @@
         </div>
 
         <div class="form-group row">
-            <div class="col-12 col-lg-6">
+            <div class="col-12">
                 <label for="departamento">Departamento</label>
                 <select class="form-control selectpicker" data-live-search="true" id="departamento" name="department_id" disabled>
                     <option value="">Seleccione</option>
@@ -196,7 +228,7 @@
                 </select>
             </div>
 
-            <div class="col-12 col-lg-6">
+            <div class="col-12">
                 <label for="localidad">Localidad</label>
                 <select class="form-control selectpicker" data-live-search="true" id="localidad" name="location_id" disabled>
                     @if ($estudiante)
@@ -215,6 +247,68 @@
                     <textarea class="form-control" id="direccion" name="address" required>{{ $estudiante->address }}</textarea>
                 @else
                     <textarea class="form-control" id="direccion" name="address" required>{{ old('address')}}</textarea>
+                @endif
+            </div>
+        </div>
+
+        <div class="form-group row">
+            <div class="col-12">
+                <hr class="linea_bordo mb-4">
+                <h6><b>DOMICILIO LEGAL (Domicilio que figura en su DNI)</b></h6>
+            </div>
+        </div>
+
+        <div class="form-group row">
+            
+            <div class="col-12">
+                <label for="provinciaL">Provincia</label>
+                <select class="form-control selectpicker" data-live-search="true" id="provinciaL" name="province_legal_id" disabled>
+                    <option value="">Seleccione</option>
+                    @foreach ($provincias as $provincia)
+                        @if ($estudiante)
+                            <option value="{{ $provincia->id }}" {{($estudiante->province_legal_id==$provincia->id)? 'selected':''}}>{{ $provincia->description }}</option>
+                        @else
+                            <option value="{{ $provincia->id }}" {{(old('province_legal_id')==$provincia->id)? 'selected':''}}>{{ $provincia->description }}</option>
+                        @endif
+                    @endforeach
+                </select>
+            </div>
+        </div>
+
+        <div class="form-group row">
+            <div class="col-12">
+                <label for="departamentoL">Departamento</label>
+                <select class="form-control selectpicker" data-live-search="true" id="departamentoL" name="department_legal_id" disabled>
+                    <option value="">Seleccione</option>
+                    @foreach ($departamentos as $departamento)
+                        @if ($estudiante)
+                            <option value="{{ $departamento->id }}" {{($estudiante->department_legal_id==$departamento->id)? 'selected':''}}>{{ $departamento->description }}</option>
+                        @else
+                            <option value="{{ $departamento->id }}" {{(old('department_legal_id')==$departamento->id)? 'selected':''}}>{{ $departamento->description }}</option>
+                        @endif
+                    @endforeach
+                </select>
+            </div>
+
+            <div class="col-12">
+                <label for="localidadL">Localidad</label>
+                <select class="form-control selectpicker" data-live-search="true" id="localidadL" name="location_legal_id" disabled>
+                    @if ($estudiante)
+                        <option value="{{ $estudiante->location_legal_id }}">{{ $estudiante->location->description }}</option>
+                    @else
+                        <option value="">Seleccione</option>
+                    @endif
+                </select>
+            </div>
+        </div>
+        
+        <div class="form-group row">
+            <div class="col-12">
+                <label for="direccionL">Dirección</label>
+                @if ($estudiante)                    
+                    <textarea class="form-control" id="direccionL" name="address_legal" required>{{ $estudiante->address_legal }}</textarea>
+                @else
+                    <textarea class="form-control" id="direccionL" name="address_legal" required>{{ old('address_legal')}}</textarea>
                 @endif
             </div>
         </div>
@@ -254,46 +348,6 @@
         </div>
 
         <div class="form-group row">
-            <div class="col-12 col-lg-8">
-                <label>2- Fotocopia de DNI (actualizado) frente y reverso.</label>
-            </div>
-
-            <div class="col-12 col-lg-4">
-                @if ($estudiante)
-                    <input type="file" class="form-control-file" name="files[]" multiple>
-                    @php 
-                        $file_fotocopia_dni = $estudiante->documentation->where('description', 'Fotocopia de DNI')->last()->src;
-                        $file = explode('/',$file_fotocopia_dni); 
-                    @endphp
-                    <a href="{{ $file_fotocopia_dni }}">{{ $file[3] }}</a>
-                @else
-                    <input type="file" class="form-control-file" name="files[]" multiple value="{{ old('files[]') }}" required>
-                @endif
-                    <input type="hidden" value="Fotocopia de DNI" name="description1">
-            </div>
-        </div>
-
-        <div class="form-group row">
-            <div class="col-12 col-lg-8">
-                <label>3- Foto Carnet color (4 x 4).</label>
-            </div>
-
-            <div class="col-12 col-lg-4">
-                @if ($estudiante)
-                    <input type="file" class="form-control-file" name="files[]" multiple>
-                    @php 
-                        $file_foto_carnet = $estudiante->documentation->where('description', 'Foto Carnet')->last()->src;
-                        $file = explode('/',$file_foto_carnet);
-                    @endphp
-                    <a href="{{ $file_foto_carnet }}">{{ $file[3] }}</a>
-                 @else
-                    <input type="file" class="form-control-file" name="files[]" multiple value="{{ old('files[]') }}" required>
-                @endif
-                    <input type="hidden" value="Foto Carnet" name="description2">
-            </div>
-        </div>
-
-        <div class="form-group row">
             <div class="col-12">
                 <hr class="linea_bordo">
             </div>
@@ -302,10 +356,10 @@
         <div class="form-group row">
             <div class="col-lg-12">
                 <p><b>COMPROMISO CON LA INSTITUCION</b></p>
-                <p>Por medio de la presente, declaro conocer y aceptar todas las disposiciones academicas y diciplinarias de los reglamentos del instituto San Martin de Porres, como asi tambien las condiciones de matriculacion, abono de cuota y otros conceptos. asi como otras resoluciones que emita la  autoridad competente y comprometo a respetarlos estrictamente.</p>
+                <p>Por medio de la presente, declaro conocer y aceptar todas las disposiciones academicas y disciplinarias de los reglamentos del instituto San Martin de Porres, como asi tambien las condiciones de matriculacion, abono de cuota y otros conceptos. asi como otras resoluciones que emita la  autoridad competente y comprometo a respetarlos estrictamente.</p>
                 <br>
                 <p><b>INFORMACION IMPORTATEN A TENER EN CUENTA</b></p>
-                <P>*La inscripcion sera validada a partir del momento en que se acredite el pago de dichp arancel. sea en el instituto o en la entidad financiera correspondiente, dentro de las fechas establecidas por la institucion y la presentacion de la documentacion correspondiente.</P>
+                <P>*La inscripcion sera validada a partir del momento en que se acredite el pago de dicho arancel. Sea en el instituto o en la entidad financiera correspondiente, dentro de las fechas establecidas por la institucion y la presentacion de la documentacion correspondiente.</P>
                 <br>
                 <p>*Por el pago de la inscripcion el ingresante tiene derecho a realizar el taller propedeutico <b>NO SIENDO EL MONTO REINTEGRABLE POR NINGUN CONCEPTO.</b> El ingresante adquiere la condicion de estudiante regular por el pago de la matricula completa, la realizacion del taller propedeutico y la presentacion de todos los requisitos establecidos por la institucion</p>
                 <br>
@@ -322,11 +376,6 @@
                     </div>
                 </div>
                 <br>
-                <div class="pull-right">
-                    <p><b>Anexo 1 de la presente</b></p>
-                    <p><a target="_blank" href="http://synergysoft.host/porres/archivos/ORGANIGRAMA_ISMP.pdf">Organigrama Institucional</a></p>
-                    <p><a target="_blank" href="http://ismp.org.ar/files/Reglamento-ISMP.pdf">Reglamento Institucional</a></p>
-                </div>
             </div>
         </div>
         
@@ -339,6 +388,7 @@
     $(function() {
         if(document.getElementById('nacionalidad').value == 1){
             $("#provincia").attr('disabled',false);
+            $("#provinciaL").attr('disabled',false);
             //$('#provincia').selectpicker('refresh');
         }
         
@@ -350,10 +400,21 @@
             $("#localidad").prop('required',true);
         }
 
+        if(document.getElementById('provinciaL').value == 1){
+            $("#departamentoL").attr('disabled',false);
+            $("#departamentoL").prop('required',true);
+
+            $("#localidadL").attr('disabled',false);
+            $("#localidadL").prop('required',true);
+        }
+
         $("#nacionalidad").change( function(event) {
             if ($(this).val() == 1) {
                 $("#provincia").attr('disabled',false);
                 $("#provincia").prop('required',true);
+
+                $("#provinciaL").attr('disabled',false);
+                $("#provinciaL").prop('required',true);
              
                 if(document.getElementById('provincia').value == 1){
                     $("#departamento").attr('disabled',false);
@@ -362,15 +423,26 @@
                     $("#localidad").attr('disabled',false);
                     $("#localidad").prop('required',true);
                 }
+
+                if(document.getElementById('provinciaL').value == 1){
+                    $("#departamentoL").attr('disabled',false);
+                    $("#departamentoL").prop('required',true);
+
+                    $("#localidadL").attr('disabled',false);
+                    $("#localidadL").prop('required',true);
+                }
             } else {
                 $("#provincia").attr('disabled',true);
                 $('#provincia').selectpicker('refresh');
 
+                $("#provinciaL").attr('disabled',true);
+                $('#provinciaL').selectpicker('refresh');
+
                 $("#departamento").attr('disabled',true);
-                //$('#departamento').selectpicker('refresh');
+                $("#departamentoL").attr('disabled',true);
 
                 $("#localidad").attr('disabled',true);
-                //$('#localidad').selectpicker('refresh');
+                $("#localidadL").attr('disabled',true);
             }
         });
  
@@ -383,22 +455,42 @@
                 $("#localidad").prop('required',true);
             } else {
                 $("#departamento").attr('disabled',true);
-                //$('#departamento').selectpicker('refresh');
 
                 $("#localidad").attr('disabled',true);
-                //$('#localidad').selectpicker('refresh');
+            }
+        });
+
+        $("#provinciaL").change(function(event){
+            if ($(this).val() == 1) {
+                $("#departamentoL").attr('disabled',false);
+                $("#departamentoL").prop('required',true);
+
+                $("#localidadL").attr('disabled',false);
+                $("#localidadL").prop('required',true);
+            } else {
+                $("#departamentoL").attr('disabled',true);
+
+                $("#localidadL").attr('disabled',true);
             }
         });
 
         $("#departamento").change(function(event){
             $("#localidad").attr('disabled',false);
-            //$("#localidad").prop('required',true);
             $.get("getLocalidades/"+event.target.value,function(response,indicador){
                 $("#localidad").empty();
                 for(i=0; i<response.length; i++){
                     $("#localidad").append("<option value='"+response[i].id+"'>"+response[i].description+"</option>");
                 }
-                //$('#localidad').selectpicker('refresh');
+            });
+        });
+
+        $("#departamentoL").change(function(event){
+            $("#localidadL").attr('disabled',false);
+            $.get("getLocalidades/"+event.target.value,function(response,indicador){
+                $("#localidadL").empty();
+                for(i=0; i<response.length; i++){
+                    $("#localidadL").append("<option value='"+response[i].id+"'>"+response[i].description+"</option>");
+                }
             });
         });
     });
