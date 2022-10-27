8e13dc5b7500c8e7b48008bae77dc26eff5974cb
martaorpi <67160498+martaorpi@users.noreply.github.com>
2022-10-26 21:15:12 -0300

    ss

--- resources/views/vendor/backpack/student_view.blade.php
+++ resources/views/vendor/backpack/student_view.blade.php
@@ -22,7 +22,7 @@
                         <div class="col-sm-6" align="right">
                             @if($entry->status == 'Solicitado')
                               @php
-                              $career = App\Models\Career::where('id', $entry->career_id)->with('students_with_space')->get();      
+                              $career = App\Models\Career::where('id', $entry->career_id)->with('students_with_space')->first();
                               @endphp
                               @if ($career->available_space > $career->students_with_space->count())
                                 <button class="btn btn-success btn-sm pl-3 pr-3 col-6" id="btnSign_up"><i class="nav-icon la la-check"></i>Alta Sistema de Cobranza</button>
