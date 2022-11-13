24ee3a0ceab9aaa334aeedde0b09ac3cb59f2c43
Marta Orpi <martich82@gmail.com>
2022-11-13 01:57:54 -0300

    ss

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/dashboard-2023';
+    public const HOME = '/dashboard';
 
     /**
      * The controller namespace for the application.
