3a0f89990833623720d1b89d5b848a8087433d89
martaorpi <67160498+martaorpi@users.noreply.github.com>
2022-12-21 18:48:00 -0300

    ss

---
+++ resources/views/estudiantes.blade.php
@@ -0,0 +1,10 @@
+<div class="card border-danger mb-3" style="max-width: 18rem;">
+    <a href="#" class="text-decoration-none">
+        <div class="card-header">PAGOS</div>
+        <div class="card-body text-danger">
+            <h5 class="card-title">Título</h5>
+            <p class="card-text">Texto.</p>
+        </div>
+    </a>
+</div>
+
