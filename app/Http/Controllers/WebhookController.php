1ed1365c39059c456e6eef1d3f71e15feb1db869
unknown <dii.renek@gmail.com>
2023-02-22 20:10:50 -0300

    fix

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -14,8 +14,8 @@ class WebhookController extends Controller
         $payment_id = $request['data']['id'];
         $response = json_decode(Http::get("https://api.mercadopago.com/v1/payments/$payment_id" . "?access_token=APP_USR-6091462099911216-022015-618419610b93c8431b635e6a46e8bb80-1314495149"));
         
-        $order = Order::find(2);
-        $order->payment_id = $response->external_reference;
+        $order = Order::find($response->external_reference);
+        $order->payment_id = $payment_id;
         $order->save();
 
         if ($response->status == "approved"){
