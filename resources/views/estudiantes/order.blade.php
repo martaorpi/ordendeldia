ac0bdc9b80c1b8377247bde403e23b65123b6e87
unknown <dii.renek@gmail.com>
2023-02-23 12:45:19 -0300

    fix

--- resources/views/estudiantes/order.blade.php
+++ resources/views/estudiantes/order.blade.php
@@ -34,6 +34,7 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
                     <p class="h3 text-center">ORDEN DE PAGO</p>
+                    {{$order}}
                     <span class="badge bg-{{$order->state->color()}}">{{ $order->state->name() }}</span>
                 </div>
             </div>
