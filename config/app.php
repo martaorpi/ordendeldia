ba9bffacc47e8cfc04b9a16cae9b04cb8832b4a9
martaorpi <67160498+martaorpi@users.noreply.github.com>
2023-03-13 10:01:54 -0300

    ss

--- config/app.php
+++ config/app.php
@@ -212,7 +212,7 @@ return [
         // 'ExampleClass' => App\Example\ExampleClass::class,
     ])->toArray(),*/
     'aliases' => [
-
+        'cors' => \Fruitcake\Cors\HandleCors::class,
         'App' => Illuminate\Support\Facades\App::class,
         'Arr' => Illuminate\Support\Arr::class,
         'Artisan' => Illuminate\Support\Facades\Artisan::class,
