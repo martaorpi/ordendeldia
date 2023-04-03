03f358e93362e9d09da43f75a4c22fa8c5d69ae1
martaorpi <martich82@gmail.com>
2023-04-03 13:22:02 -0300

    ss

--- resources/views/digesto.blade.php
+++ resources/views/digesto.blade.php
@@ -285,7 +285,7 @@
                 <a href="{{ asset('files/UTP/Ley 26.657 - Ley de Salud Mental.pdf')}}" target="blank">* Ley Nacional N° 26657 - Ley de Salud Mental.</a><br>
                 <a href="{{ asset('files/UTP/DICTADO DE SENTENCIA - CASAGRANDE OCTAVIA Y OTROS C ANSES Y OTROS S REAJUSTES VARIOS - UTP.pdf')}}" target="blank">* Dictado de sentencia caso CASAGRANDE "EXPTE N° 44515/2013 CASAGRANDE OCTAVIO Y OTROS C/ANSES Y OTRO S/REAJUSTES VARIOS.</a><br>
                 <a href="{{ asset('files/UTP/Dictado de Sentencia Caso LEGUIZAMÓN OSCAR ISMAEL C ADMINISTRACIÓN NACIONAL DE LA SEGURIDAD SOCIAL (ANSES) Y OTRO S REAJUSTES VARIOS.pdf')}}" target="blank">* Dictado de sentencia caso LEGUIZAMÓN OSCAR ISMAEL C/ADMINISTRACIÓN NACIONAL DE LA SEGURIDAD SOCIAL (ANSES) Y OTRO S/ REAJUSTES VARIOS.</a><br>
-                <a href="{{ asset('files/UTP/Convención Americana sobre Derechos Humanos (1).pdf')}}" target="blank">* Convención americana sobre Derechos Humanos (Pacto de San José de Costa Rica).</a><br>
+                <a href="{{ asset('files/Normativa_General/Nacional_Internacional/Convención Americana sobre Derechos Humanos.pdf')}}" target="blank">* Convención americana sobre derechos humanos (Pacto San José de Costa Rica).</a><br>
                 <a href="{{ asset('files/UTP/REGLAMENTO - INSTRUCTIVO DE TRABAJO - UTP.pdf')}}" target="blank">* Resolución de ANSES N° 540/07 pautas para creación de U.T.P. y lineamientos de trabajo.</a>
                 <br><br>
             </p>
@@ -329,6 +329,101 @@
         <div class="accordion-content">
             <p class="ml-3">
                 <a href="{{ asset('files/Reconocimiento_Ciudadano/Ley 25326 - PROTECCION DATOS PERSONALES.pdf')}}" target="blank">* Ley 25326 de protección de datos personales.</a>
+                <br><br>
+            </p>
+        </div>
+    </div>
+    <div class="accordion">
+        <input type="radio" name="radio-a" id="check10">
+        <label class="accordion-label" for="check10">Oficina Santiago Acompaña</label>
+        <div class="accordion-content">
+            <p class="ml-3">
+                <a href="{{ asset('files/Normativa_General/Nacional_Internacional/LEY Nº 26.485 - VIOLENCIA CONTRA LA MUJER.pdf')}}" target="blank">* Ley Nacional 26485 de protección integral a las mujeres.</a><br>
+                <a href="{{ asset('files/Normativa_General/Nacional_Internacional/Ley Nacional 27499 - Micaela.pdf')}}" target="blank">* Ley Nacional 27499 Micaela.</a><br>
+                <a href="{{ asset('files/Santiago_A/Ley 7032.pdf')}}" target="blank">* Ley Provincial 7032 prevención, sanción y erradicación de la violencia contra las mujeres.</a><br>
+                <a href="{{ asset('files/Santiago_A/CONVENIO ENTRE MINISTERIO DE JUSTICIA Y DDHH Y EL MINISTERIO DE SEG Y CULTO - OF SGO ACOMPAÑA.pdf')}}" target="blank">* Convenio entre Ministerio de Justicia y DDHH y el Ministerio de Seguridad y culto.</a>
+                <br><br>
+            </p>
+        </div>
+    </div>
+    <div class="accordion">
+        <input type="radio" name="radio-a" id="check11">
+        <label class="accordion-label" for="check11">División Secuestros Judiciales</label>
+        <div class="accordion-content">
+            <p class="ml-3">
+                <a href="{{ asset('files/Secuestros/Ley 20785 - BIENES OBJETO DE SECUESTRO.pdf')}}" target="blank">* Ley 20785 bienes objeto de secuestro en causas penales. Custodia y disposición.</a>
+                <br><br>
+            </p>
+        </div>
+    </div>
+    <div class="accordion">
+        <input type="radio" name="radio-a" id="check12">
+        <label class="accordion-label" for="check12">Centro Único de Detenidos</label>
+        <div class="accordion-content">
+            <p class="ml-3">
+                <a href="{{ asset('files/Normativa_General/Nacional_Internacional/Declaración Universal de Derechos Humanos y Declaración Americana de los Derechos Hombre.pdf')}}" target="blank">* Declaración universal de derechos humanos y declaración americana de los derechos del hombre.</a><br>
+                <a href="{{ asset('files/Normativa_General/Nacional_Internacional/Convención Americana sobre Derechos Humanos.pdf')}}" target="blank">* Convención americana sobre derechos humanos.</a><br>
+                <a href="{{ asset('files/Normativa_General/Nacional_Internacional/Convención sobre los derechos del Niño.pdf')}}" target="blank">* Convención sobre los derechos del niño.</a><br>
+                <a href="" target="blank">* Reglas mínimas para el tratamiento de los reclusos.</a><br>
+                <a href="{{ asset('files/CUD/Reglamento General de Procesados.pdf')}}" target="blank">* Reglamento general de procesados - Decreto N° 303/96 modificado por Decreto N° 1464/07.</a><br>
+                <a href="{{ asset('files/CUD/Reglamento de las Modalidades Básicas de la Ejecución Penal.pdf')}}" target="blank">* Reglamento de las modalidades básicas de la ejecución penal - Decreto N° 396/99.</a><br>
+                <a href="{{ asset('files/CUD/Reglamento de Disciplina para los Internos.pdf')}}" target="blank">* Reglamento de disciplina para los internos - Decreto N° 18/97.</a><br>
+                <a href="{{ asset('files/CUD/Reglamento de Comunicaciones de los Internos.pdf')}}" target="blank">* Reglamento de comunicaciones de los internos - Decreto N° 1136/97..</a>
+                <br><br>
+            </p>
+        </div>
+    </div>
+    <div class="accordion">
+        <input type="radio" name="radio-a" id="check13">
+        <label class="accordion-label" for="check13">Dirección General de Recursos Humanos</label>
+        <div class="accordion-content">
+            <p class="ml-3">
+                <a href="{{ asset('files/Normativa_General/Provincial/LEY 4793 - ORGANICA DE LA POLICIA DE LA PROVINCIA.pdf')}}" target="blank">* Ley 4793 - orgánica de la policía de la provincia.</a><br>
+                <a href="{{ asset('files/Normativa_General/Provincial/DEC REGLAMENTARIO 4793 - ORGANICA DE LA POLICIA DE LA PROVINCIA.pdf')}}" target="blank">* Decreto reglamentario 4793 - orgánica de la policía de la provincia.</a><br>
+                <a href="{{ asset('files/Normativa_General/Provincial/LEY 4794 - GENERALIDADES PARA EL PERSONAL POLICIAL.pdf')}}" target="blank">* Ley 4794 - generalidades para el personal policial.</a><br>
+                <a href="{{ asset('files/Normativa_General/Provincial/DEC REGLAMENTO 4794 - GENERALIDADES PARA EL PERSONAL POLICIAL.pdf')}}" target="blank">* Decreto reglamento 4794 - generalidades para el personal policial.</a>
+                <br><br>
+            </p>
+        </div>
+    </div>
+    <div class="accordion">
+        <input type="radio" name="radio-a" id="check13">
+        <label class="accordion-label" for="check13">Departamento de Reconocimiento Médico</label>
+        <div class="accordion-content">
+            <p class="ml-3">
+                <a href="{{ asset('files/Normativa_General/Provincial/LEY 4794 - GENERALIDADES PARA EL PERSONAL POLICIAL.pdf')}}" target="blank">* Ley 4794 - generalidades para el personal policial.</a><br>
+                <a href="{{ asset('files/Normativa_General/Provincial/DEC REGLAMENTO 4794 - GENERALIDADES PARA EL PERSONAL POLICIAL.pdf')}}" target="blank">* Decreto reglamento 4794 - generalidades para el personal policial.</a><br>
+                <a href="{{ asset('files/Reconocimiento_Medico/LEY PROVINCIAL 5711 - Sistema de Protección Integral del Discapacitado y su Familia.pdf')}}" target="blank">* Ley Provincial N° 5711 "Sistema de protección integral del discapacitado y su familia".</a><br>
+                <a href="{{ asset('files/Reconocimiento_Medico/LEY 24557 - RIESGOS DEL TRABAJO.pdf')}}" target="blank">* Ley de Riesgo de Trabajo N° 24.557.</a><br>
+                <a href="{{ asset('files/Reconocimiento_Medico/Decreto Nacional 659_96 - Tabla de Incapacidades.pdf')}}" target="blank">* Decreto N° 659/1996.</a><br>
+                <a href="{{ asset('files/Reconocimiento_Medico/Ley 24.241 - Sistema Integrado de Jubilaciones y Pensiones.pdf')}}" target="blank">* Ley Previsional N° 24241.</a><br>
+                <a href="{{ asset('files/Reconocimiento_Medico/Decreto N° 478-98 - SISTEMA INTEGRADO DE JUBILACIONES Y PENSIONES.pdf')}}" target="blank">* Decreto N° 478/98.</a>
+                <br><br>
+            </p>
+        </div>
+    </div>
+    <div class="accordion">
+        <input type="radio" name="radio-a" id="check13">
+        <label class="accordion-label" for="check13">División Prevención y Protección contra el Alcoholismo</label>
+        <div class="accordion-content">
+            <p class="ml-3">
+                <a href="{{ asset('files/Div_Alcoholismo/Ordenanza N° 418-205.pdf')}}" target="blank">* Ordenanza N° 418/05 ciudad de La Banda – Código de Faltas Municipal.</a><br>
+                <a href="{{ asset('files/Planeamiento/LEY 24788 - LEY NACIONAL DE LUCHA CONTRA EL ALCOHOLISMO.pdf')}}" target="blank">* Ley Nacional de lucha contra el alcoholismo 24788.</a><br>
+                <a href="{{ asset('files/Div_Alcoholismo/Ley provincial n 4547 VENTA Y-O EXPENDIO DE BEBIDAS ALCOHOLICAS AL MENUDEO.pdf')}}" target="blank">* Ley provincial n° 4547 venta y/o expendio bebidas alcohólicas al menudeo de la provincia y su decreto reglamentario.</a><br>
+                <a href="{{ asset('files/Div_Alcoholismo/Ley N 7163 - Prohibición de venta, expendio o suministro de bebidas alcohólicas entre las 24 y las 9 hs.pdf')}}" target="blank">* Ley provincial n° 7163 – prohibición de venta, expendio o suministro de bebidas alcohólicas entre las 24 y las 9 hs.</a><br>
+                <a href="{{ asset('files/Normativa_General/Provincial/CODIGO DE FALTAS DE LA PROVINCIA DE SANTIAGO DEL ESTERO.pdf')}}" target="blank">* Código de faltas provincial.</a>
+                <br><br>
+            </p>
+        </div>
+    </div>
+    <div class="accordion">
+        <input type="radio" name="radio-a" id="check14">
+        <label class="accordion-label" for="check14">Dirección General de Policía Cinetífica</label>
+        <div class="accordion-content">
+            <p class="ml-3">
+                <a href="{{ asset('files/Cientifica/Manual_actuacion_lugar_hecho_escena_delito - DGPC.pdf')}}" target="blank">* Manual de actuación en el lugar del hecho y/o escena del delito.</a><br>
+                <a href="{{ asset('files/Cientifica/Protocolo unificado de los MP de Arg.pdf')}}" target="blank">* Protocolo unificado de los Ministerios Públicos de la República Argentina.</a><br>
+                <a href="{{ asset('files/Cientifica/Guía para el relevamiento y conservación de la evidencia.pdf')}}" target="blank">* Guía para el relevamiento y conservación de la evidencia.</a>
                 <br><br>
             </p>
         </div>
